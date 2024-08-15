2d0dcc145e0cf7aa8a4db8a9854d3a21d8b16209
Mylena <mylenadiovanna829@gmail.com>
2024-08-15 19:26:04 -0300

    aulajs

---
+++ aula.js
@@ -0,0 +1,15 @@
+var nome = "Mylena"; //string
+var  idade = 12; //number inteiro
+var peso = 43.7; // number - float
+var acordado = true; //boolean
+
+var cidades = ["curitiba", "pinhais", "colombo"]; // array
+
+if (nome == idade)
+{
+    console.log("verdade")
+
+} else {
+
+    console.log("falso")
+}
