bd00af68bdef39a945e07290410ff66b7b37a1a9
Mylena <mylenadiovanna829@gmail.com>
2024-08-15 21:29:47 -0300

    aula 2

---
+++ aula2.js
@@ -0,0 +1,12 @@
+let nome = 123;
+// verificar o tipo da variavel 
+console.log( typeof nome );
+
+let cidades = ["cutirba", "pinhais"];
+console.log( cidade );
+
+function Caneta()
+{
+    this.caneta = "azul";
+}
+
